25cbedbd6c2c561f254156243eb7c65b833a8db5
Nur Esa Mahendra <Respectunity17@gmail.com>
2021-10-30 09:40:08 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Coba Git remote</title>
 </head>
+
 <body>
   <h1>Hello World</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aperiam alias consequatur nostrum cumque dicta quod
+    blanditiis voluptate quas quibusdam qui eligendi, molestias autem facere. Fugit nemo excepturi voluptatem eaque.</p>
 </body>
+
 </html>
